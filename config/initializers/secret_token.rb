bd7fbfae235217eb8beb25a7175d5abe0bbc4236
Fida <svains@gmail.com>
2012-07-11 11:40:47 +0200

    generated new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thrifter::Application.config.secret_token = '10d729e6df8fc83110cb351dbf4559dce64fe0d3bc6d258bc8e41581152ae62dac4214b882823cf5513fd9bddbc91a5718c40cb8fce84523d2f76fb40908891c'
