1aa1d889851fe85a0cc31838ab3e4e1c45a44ec8
Fida <svains@gmail.com>
2012-07-16 16:50:59 +0200

    add brand form views and controller

--- app/controllers/admin/brands_controller.rb
+++ app/controllers/admin/brands_controller.rb
@@ -1,9 +1,32 @@
 class Admin::BrandsController < Admin::AdminController
+  before_filter :load_object
+
   def index
 
   end
 
   def new
 
+  end
+
+  def create
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+  def load_object
+    @brand = Brand.find(params[:id]) unless params[:id].blank?
+    @brand ||= Brand.new(params[:brand])
   end
 end
