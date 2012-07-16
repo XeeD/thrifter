252501b2fc4a00bc63080ae888739b617add85c4
Fida <svains@gmail.com>
2012-07-16 16:52:45 +0200

    add brand resource into routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Thrifter::Application.routes.draw do
   #Admin backend
   namespace :admin do
     root :to => "admin#index"
-    resources :brands#, :path => "znacky"
+    resources :brands, :path => "znacky", :path_names => {:new => "nova", :edit => "editace"}
   end
 end
