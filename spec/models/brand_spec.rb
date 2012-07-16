3368b2184758eca467ec87b9a67af5c1810845d2
Fida <svains@gmail.com>
2012-07-16 13:52:05 +0200

    add brand model and specs for it

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -1,7 +1,23 @@
 require 'spec_helper'
 
 describe Brand do
-  it "is valid with valid attributes" do
-    Brand.new.should be_valid
+  setup do
+    @brand = Brand.new
+  end
+
+  it "should not be valid without name" do
+    @brand.new.should_not_be_valid
+  end
+
+  it "should not be valid without url" do
+
+  end
+
+  it "should not be valid without description" do
+
+  end
+
+  it "should be valid with all required attributes" do
+
   end
 end
