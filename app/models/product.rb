310c38f0abf5cf675ac806e5c898a9e8c52ec57c
XeeD <xeedus@gmail.com>
2012-07-18 22:29:35 +0200

    add validations to Product model
    Also improved and fixed the examples for those validations.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,14 @@
 class Product < ActiveRecord::Base
-
+  validates :name, presence: true, length: {maximum: 301}
+  validates :model_name, presence: true, length: {maximum: 150}
+  validates :url, presence: true, length: {maximum: 301}, uniqueness: true
+  validates :short_description, presence: true
+  validates :description, presence: true
+  validates :default_price, presence: true, numericality: {only_integer: true}
+  validates :recommended_price, presence: true, numericality: {only_integer: true}
+  validates :purchase_price, presence: true, numericality: {only_integer: true}
+  validates :warranty, presence: true, numericality: {only_integer: true}
+  validates :recycling_fee, presence: true, numericality: {only_integer: true}
+  validates :vat_rate, presence: true, numericality: {only_integer: true}
+  validates :external_id, presence: true, numericality: {only_integer: true}
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,6 +1,25 @@
+# encoding: UTF-8
 require 'spec_helper'
 
 describe Product do
+
+  def valid_attributes
+    {
+        name: "LG GB3133TIJW",
+        model_name: "GB3133TIJW",
+        short_description: "Chladnička",
+        description: "No-Frost chladnička...",
+        default_price: "12990",
+        recommended_price: "15990",
+        purchase_price: "9000",
+        recycling_fee: "217",
+        vat_rate: "20",
+        external_id: "2123456",
+        url: "lg-gb3133tijw",
+        gray_import: "",
+        warranty: "24"
+    }
+  end
 
   # Validations
   # name
@@ -9,12 +28,15 @@ describe Product do
 
   # model_name
   it { should validate_presence_of(:model_name) }
-  it { should ensure_length_of(:name).is_at_most(150) }
+  it { should ensure_length_of(:model_name).is_at_most(150) }
 
   # url
   it { should validate_presence_of(:url) }
-  it { should ensure_length_of(:url).is_at_most(300) }
-  it { should validate_uniqueness_of(:url) }
+  it { should ensure_length_of(:url).is_at_most(301) }
+  it {
+    Product.create(valid_attributes)
+    should validate_uniqueness_of(:url)
+  }
 
   # short_description
   it { should validate_presence_of(:short_description) }
@@ -27,7 +49,7 @@ describe Product do
   it { should validate_numericality_of(:default_price) }
 
   # recommended_price
-  it { should validate_presence_of(:recomended_price) }
+  it { should validate_presence_of(:recommended_price) }
   it { should validate_numericality_of(:recommended_price) }
 
   # purchase_price
@@ -50,12 +72,10 @@ describe Product do
   it { should validate_presence_of(:external_id) }
   it { should validate_numericality_of(:external_id) }
 
-  # state
-  it { should ensure_inclusion_of(:state, in: [:new]) }
-
   context "with valid attributes" do
     it "should be valid" do
-      Brand.new(
+      pending
+      Products.new(
           name: "",
           model_name: "",
           short_description: "",
