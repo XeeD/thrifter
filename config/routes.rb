c0d079265ac9efeee5f8566738ddb450e61fb2a3
Fida <svains@gmail.com>
2012-07-18 14:54:38 +0200

    update brand resource not to route show action

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Thrifter::Application.routes.draw do
   namespace :admin do
     root :to => "admin#index"
 
-    resources :brands,   :path => "znacky",   :path_names => {:new => "nova", :edit => "editace"}
+    resources :brands,   :path => "znacky",   :path_names => {:new => "nova", :edit => "editace"}, :except => "show"
     resources :products, :path => "produkty", :path_names => {:new => "novy", :edit => "editace"}
   end
 end
