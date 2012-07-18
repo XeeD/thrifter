27b0397b9df60183dd561b0fb0df4efcff57f68d
XeeD <xeedus@gmail.com>
2012-07-18 15:25:02 +0200

    add migration to create products table

---
+++ db/migrate/20120718130444_create_products.rb
@@ -0,0 +1,39 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products, :force => true do |t|
+      t.string :name, limit: 300
+      t.string :model_name, limit: 150
+      t.string :url, limit: 300
+
+      t.integer :external_id, limit: 7
+      t.string :ean_code, limit: 13, null: false
+
+      t.text :short_description
+      t.text :description
+
+      t.integer :default_price, limit: 7
+      t.integer :recommended_price, limit: 7
+      t.integer :purchase_price, limit: 7
+      t.integer :recycling_fee, limit: 4
+      t.integer :waranty, limit: 3
+      t.decimal :vat_rate, :scale => 1, :precision => 3
+
+      t.string :state, default: :new
+      t.text :admin_comment
+      t.string :initial_data_source, default: :manually_added
+
+      t.boolean :grey_import, default: false
+      t.boolean :top_product, default: false
+
+      t.timestamps
+
+      # Associations
+      t.belongs_to :brand
+
+    end
+
+    %w(name model_name url external_id initial_data_source top_product brand_id).each do |index|
+      add_index :products, index.to_sym
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,44 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120716112101) do
+ActiveRecord::Schema.define(:version => 20120718130444) do
 
   create_table "brands", :force => true do |t|
     t.string "name",        :limit => 100
     t.string "url",         :limit => 100
     t.text   "description"
   end
+
+  create_table "products", :force => true do |t|
+    t.string   "name",                :limit => 300
+    t.string   "model_name",          :limit => 150
+    t.string   "url",                 :limit => 300
+    t.integer  "external_id",         :limit => 8
+    t.string   "ean_code",            :limit => 13,                                                              :null => false
+    t.text     "short_description"
+    t.text     "description"
+    t.integer  "default_price",       :limit => 8
+    t.integer  "recommended_price",   :limit => 8
+    t.integer  "purchase_price",      :limit => 8
+    t.integer  "recycling_fee"
+    t.integer  "waranty",             :limit => 3
+    t.decimal  "vat_rate",                           :precision => 3, :scale => 1
+    t.string   "state",                                                            :default => "new"
+    t.text     "admin_comment"
+    t.string   "initial_data_source",                                              :default => "manually_added"
+    t.boolean  "grey_import",                                                      :default => false
+    t.boolean  "top_product",                                                      :default => false
+    t.datetime "created_at",                                                                                     :null => false
+    t.datetime "updated_at",                                                                                     :null => false
+    t.integer  "brand_id"
+  end
+
+  add_index "products", ["brand_id"], :name => "index_products_on_brand_id"
+  add_index "products", ["external_id"], :name => "index_products_on_external_id"
+  add_index "products", ["initial_data_source"], :name => "index_products_on_initial_data_source"
+  add_index "products", ["model_name"], :name => "index_products_on_model_name"
+  add_index "products", ["name"], :name => "index_products_on_name"
+  add_index "products", ["top_product"], :name => "index_products_on_top_product"
+  add_index "products", ["url"], :name => "index_products_on_url"
 
 end
