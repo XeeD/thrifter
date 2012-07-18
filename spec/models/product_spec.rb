e1843dcefe14064a6d978776fc50c495b1a21123
XeeD <xeedus@gmail.com>
2012-07-18 16:33:42 +0200

    improve Product model specs
    Now they work - used bad matchers previously.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,15 +5,15 @@ describe Product do
   # Validations
   # name
   it { should validate_presence_of(:name) }
-  it { should validate_length_of(:name, maximum: 301) }
+  it { should ensure_length_of(:name).is_at_most(301) }
 
   # model_name
   it { should validate_presence_of(:model_name) }
-  it { should validate_length_of(:name, maximum: 150) }
+  it { should ensure_length_of(:name).is_at_most(150) }
 
   # url
   it { should validate_presence_of(:url) }
-  it { should validate_length_of(:url, maximum: 301) }
+  it { should ensure_length_of(:url).is_at_most(300) }
   it { should validate_uniqueness_of(:url) }
 
   # short_description
@@ -24,34 +24,34 @@ describe Product do
 
   # default_price
   it { should validate_presence_of(:default_price) }
-  it { should validate_numericality_of(:default_price).only_integer.less_than(1_000_000) }
+  it { should validate_numericality_of(:default_price) }
 
   # recommended_price
   it { should validate_presence_of(:recomended_price) }
-  it { should validate_numericality_of(:recommended_price).only_integer.less_than(1_000_000) }
+  it { should validate_numericality_of(:recommended_price) }
 
   # purchase_price
   it { should validate_presence_of(:purchase_price) }
-  it { should validate_numericality_of(:purchase_price).only_integer.less_than(1_000_000) }
+  it { should validate_numericality_of(:purchase_price) }
 
   # warranty
   it { should validate_presence_of(:warranty) }
-  it { should validate_numericality_of(:warranty).only_integer.less_than(120) }
+  it { should validate_numericality_of(:warranty) }
 
   # recycling_fee
   it { should validate_presence_of(:recycling_fee) }
-  it { should validate_numericality_of(:recycling_fee).only_integer.less_than(500) }
+  it { should validate_numericality_of(:recycling_fee) }
 
   # vat_rate
   it { should validate_presence_of(:vat_rate) }
-  it { should validate_numericality_of(:vat_rate).less_than(100) }
+  it { should validate_numericality_of(:vat_rate) }
 
   # external_id
   it { should validate_presence_of(:external_id) }
-  it { should validate_numericality_of(:external_id).only_integer.less_than(10_000_000) }
+  it { should validate_numericality_of(:external_id) }
 
   # state
-  it { should validate_inclusion_of(:state, in: [:new]) }
+  it { should ensure_inclusion_of(:state, in: [:new]) }
 
   context "with valid attributes" do
     it "should be valid" do
