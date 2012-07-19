5c75232c447dad07b150b5c9323f844ecdbde599
Fida <svains@gmail.com>
2012-07-19 11:01:39 +0200

    add category migration

---
+++ db/migrate/20120719081039_create_categories.rb
@@ -0,0 +1,26 @@
+class CreateCategories < ActiveRecord::Migration
+  def change
+    create_table :categories, force: true do |t|
+      t.string :short_name,    limit: 80
+      t.string :url,           limit: 120
+      t.string :plural_name,   limit: 120
+      t.string :singular_name, limit: 120
+      t.string :category_type, limit: 20
+
+      # awesome_nested_set
+      t.integer :lft
+      t.integer :rgt
+      t.integer :parent_id
+      t.integer :depth
+
+      # Associations
+      t.belongs_to :shop
+    end
+
+    add_index :categories, :url, unique: true
+
+    %w(lft rgt parent_id depth).each do |column|
+      add_index :categories, column.to_sym
+    end
+  end
+end
