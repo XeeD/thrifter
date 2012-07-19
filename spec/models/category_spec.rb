bccc001396b2bc522b95424aa1f5ea04deccfa2c
Fida <svains@gmail.com>
2012-07-19 16:36:20 +0200

    add category model spec test

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,24 @@
+# encoding: UTF-8
 require 'spec_helper'
 
 describe Category do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # Validations
+  # short_name
+  it { should validate_presence_of(:short_name) }
+  it { should ensure_length_of(:short_name).is_at_most(80) }
+
+  # url
+  it { should validate_presence_of(:url) }
+  it { should ensure_length_of(:url).is_at_most(120) }
+
+  # plural_name
+  it { should validate_presence_of(:plural_name) }
+  it { should ensure_length_of(:plural_name).is_at_most(120) }
+
+  # singular_name
+  it { should ensure_length_of(:singular_name).is_at_most(120) }
+
+  # category_type
+  it { should validate_inclusion_of(:category_type) }
 end
