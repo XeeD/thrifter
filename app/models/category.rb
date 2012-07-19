2427e0e9dd94096ce44c312154b286412c6299d8
Fida <svains@gmail.com>
2012-07-19 16:31:30 +0200

    add category model validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,12 @@
+# encoding: UTF-8
+
 class Category < ActiveRecord::Base
+  CATEGORY_TYPES = {"Navigační" => "navigational", "Produktová" => "product_list", "Přídavná" => "additional"}
+
+  validates :short_name, :url, :plural_name, :category_type, presence: true
+  validates :short_name, length: {maximum: 80}
+  validates :url, length: {maximum: 120}
+  validates :plural_name, length: {maximum: 120}
+  validates :singular_name, length: {maximum: 120}
+  validates :category_type, inclusion: {in: CATEGORY_TYPES.values}
 end
