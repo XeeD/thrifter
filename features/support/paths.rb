fa70eadf4e3fbe01f3a52a21f1301f450cd3b2bb
Fida <svains@gmail.com>
2012-07-19 10:59:56 +0200

    remove not used form path helper

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -2,15 +2,9 @@
 
 def get_section_url(section)
   case section
-    when "administrace produktů" then admin_products_path
-    when "administrace značek"   then admin_brands_path
+    when "administrace produktů"  then admin_products_path
+    when "administrace značek"    then admin_brands_path
+    when "administrace kategorií" then admin_categories_path
     else raise "section url for '#{section}' was not found"
-  end
-end
-
-def get_form_url(form)
-  case form
-    when "přidání nové značky" then "/admin/new"
-    else raise "form url for '#{form}' was not found"
   end
 end
