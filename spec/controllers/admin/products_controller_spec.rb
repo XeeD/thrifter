bfc614cb131373d76dbea6107b55dd62f9421520
XeeD <xeedus@gmail.com>
2012-07-19 09:06:18 +0200

    add test for index and new actions of Admin::ProductsController

--- spec/controllers/admin/products_controller_spec.rb
+++ spec/controllers/admin/products_controller_spec.rb
@@ -2,9 +2,27 @@ require 'spec_helper'
 
 module Admin
   describe ProductsController do
-    it "should render index" do
-      get :index
-      response.should be_success
+    context "GET index" do
+      it "renders index" do
+        get :index
+        response.should be_success
+      end
+    end
+
+    context "GET new" do
+      it "renders new action" do
+        get :new
+        response.should be_success
+      end
+
+      context "when rendering views" do
+        render_views
+        
+        it "calls Product#new" do
+          get :new
+          Product.should_receive(:new).with(no_args).once
+        end
+      end
     end
   end
 end
