33455593eec905ed4c59902df07c79e846bf5d18
Fida <svains@gmail.com>
2012-07-20 16:55:09 +0200

    add integrity constraints

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 describe Category do
 
   let(:category) { mock_model(Category).as_null_object }
+  let(:subcategory) { mock_model(Category).as_null_object }
 
   # Associations
   it { should have_many(:categorizations) }
@@ -29,6 +30,65 @@ describe Category do
 
   # category_type
   it { should ensure_inclusion_of(:category_type).in_array(Category::CATEGORY_TYPES.values) }
+
+  describe "integrity constraints" do
+    context "for navigational category type" do
+      before do
+        subcategory.stub(category_type: "navigational")
+        category.stub(:parent_category).and_return(subcategory)
+      end
+
+      it "doesn't allow navigational category_type for subcategory" do
+        category.should_not allow_value("navigational").for(:category_type)
+      end
+
+      it "allows product_list category_type for subcategory" do
+        category.should allow_value("product_list").for(:category_type)
+      end
+
+      it "allows additional category_type for subcategory" do
+        category.should allow_value("additional").for(:category_type)
+      end
+    end
+
+    context "for product_list category type" do
+      before do
+        subcategory.stub(:category_type).and_return("product_list")
+        category.stub(:parent_category).and_return(subcategory)
+      end
+
+      it "doesn't allow navigational category_type for subcategory" do
+        category.should_not allow_value("navigational").for(:category_type)
+      end
+
+      it "doesn't allow product_list category_type for subcategory" do
+        category.should_not allow_value("category_list").for(:category_type)
+      end
+
+      it "allows additional category_type for subcategory" do
+        category.should allow_value("additional").for(:category_type)
+      end
+    end
+
+    context "for additional category type" do
+      before do
+        subcategory.stub(:category_type).and_return("additional")
+        category.stub(:parent_category).and_return(subcategory)
+      end
+
+      it "doesn't allow navigational category_type for subcategory" do
+        category.should_not allow_value("navigational").for(:category_type)
+      end
+
+      it "doesn't allow product_list category_type for subcategory" do
+        category.should_not allow_value("product_list").for(:category_type)
+      end
+
+      it "allows additional category_type for subcategory" do
+        category.should allow_value("additional").for(:category_type)
+      end
+    end
+  end
 
   context "with valid attributes" do
     it "should be valid" do
