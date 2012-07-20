76264a4dc7d687d767c52a9901057fc86b8c868b
Fida <svains@gmail.com>
2012-07-20 09:52:01 +0200

    comment out database_cleaner from rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,18 +37,18 @@ RSpec.configure do |config|
   config.order = "random"
 
   #DATABASE CLEANER SETUP
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
+  #config.before(:suite) do
+  #  DatabaseCleaner.strategy = :transaction
+  #  DatabaseCleaner.clean_with(:truncation)
+  #end
 
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
+  #config.before(:each) do
+  #  DatabaseCleaner.start
+  #end
 
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
+  #config.after(:each) do
+  #  DatabaseCleaner.clean
+  #end
 
   config.include CustomMatchers
 end
