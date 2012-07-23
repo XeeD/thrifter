7d70070bc93e6e596271e5c55db3145e454446ea
Fida <svains@gmail.com>
2012-07-23 17:17:05 +0200

    add param template model spec test

---
+++ spec/models/param_template_spec.rb
@@ -0,0 +1,20 @@
+# encoding: UTF-8
+require 'spec_helper'
+
+describe ParamTemplate do
+
+  # Associations
+  it { should belong_to(:category) }
+  it { should have_many(:param_groups) }
+
+  # Validations
+  # name
+  it { should validate_presence_of(:name) }
+  it { should ensure_length_of(:name).is_at_most(100) }
+
+  context "with valid attributes" do
+    it "should be valid" do
+      ParamTemplate.new(valid_param_template_attributes).should be_valid
+    end
+  end
+end
