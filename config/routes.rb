ccbd2874ef2c2980a97be076e30171bf10c2f366
Fida <svains@gmail.com>
2012-07-23 17:08:02 +0200

    add param templates path to routing table

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Thrifter::Application.routes.draw do
   namespace :admin do
     root :to => "admin#homepage"
 
-    resources :brands,     :path => "znacky",    :path_names => {:new => "nova", :edit => "editace"}, :except => :show
-    resources :products,   :path => "produkty",  :path_names => {:new => "novy", :edit => "editace"}, :except => :show
-    resources :categories, :path => "kategorie", :path_names => {:new => "nova", :edit => "editace"}, :except => :show
+    resources :brands,     path: "znacky",    path_names: {new: "nova", edit: "editace"}, except: :show
+    resources :products,   path: "produkty",  path_names: {new: "novy", edit: "editace"}, except: :show
+    resources :categories, path: "kategorie", path_names: {new: "nova", edit: "editace"}, except: :show
+    resources :param_templates, path: "sablony-parametru", path_names: {new: "nova", edit: "editace"}, except: :show
   end
 end
