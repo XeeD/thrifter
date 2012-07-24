8a6a99f7ad98bb5529f4304fd3f833d23b5d2b35
XeeD <xeedus@gmail.com>
2012-07-24 14:15:26 +0200

    add admin_shops_path to the paths step definition

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -6,6 +6,7 @@ def get_section_url(section)
     when "administrace značek"    then admin_brands_path
     when "administrace kategorií" then admin_categories_path
     when "administrace šablon parametrů" then admin_param_templates_path
+    when "administrace obchodů" then admin_shops_path
     else raise "section url for '#{section}' was not found"
   end
 end
