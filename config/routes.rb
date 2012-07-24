c959bcef78bc5972406247573f98859cbbce7bb0
XeeD <xeedus@gmail.com>
2012-07-24 19:38:59 +0200

    add nested resource :groups to resource :param_templates
    The resource maps to Admin::ParamGroups controller, but the name is
    shorter to be usable in paths.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Thrifter::Application.routes.draw do
     resources :brands, path: "znacky", path_names: {new: "nova", edit: "editace"}, except: :show
     resources :products, path: "produkty", path_names: {new: "novy", edit: "editace"}, except: :show
     resources :categories, path: "kategorie", path_names: {new: "nova", edit: "editace"}, except: :show
-    resources :param_templates, path: "sablony-parametru", path_names: {new: "nova", edit: "editace"}
+    resources :param_templates, path: "sablony-parametru", path_names: {new: "nova", edit: "editace"} do
+      resources :groups, path: "skupiny", path_names: {new: "nova", edit: "editace"}, :controller => "param_groups"
+    end
     resources :shops, path: "obchody", path_names: {new: "novy", edit: "editace"}, except: :show do
       member do
         get :deletion_confirmation
