d0840d31aa52df4d78bf05e5071101e7393248d8
XeeD <xeedus@gmail.com>
2012-07-24 18:38:47 +0200

    add validation of uniqueness to ParamTemplate

--- spec/models/param_template_spec.rb
+++ spec/models/param_template_spec.rb
@@ -11,6 +11,7 @@ describe ParamTemplate do
   # name
   it { should validate_presence_of(:name) }
   it { should ensure_length_of(:name).is_at_most(100) }
+  it { should validate_uniqueness_of(:name) }
 
   context "with valid attributes" do
     it "should be valid" do
