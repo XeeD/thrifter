3b96de3bb64fbb9389f5c23f72e517920bef88aa
Fida <svains@gmail.com>
2012-07-25 15:51:10 +0200

    refactor custom nested check_boxes class

--- app/inputs/check_boxes_input.rb
+++ app/inputs/check_boxes_input.rb
@@ -1,3 +1,13 @@
+=begin
+  Attribute :nested_set => true
+  =============================
+  provides nested check_boxes on model records
+
+  Attribute :show_if
+  ==================
+  calls provided method on associated model and based on result displays
+  check_box for current choice
+=end
 class CheckBoxesInput < Formtastic::Inputs::CheckBoxesInput
 
   def to_html
@@ -9,30 +19,38 @@ class CheckBoxesInput < Formtastic::Inputs::CheckBoxesInput
   end
 
   def nested_wrapping
+    @model = sanitized_method_name.singularize.classify.constantize
+
     choices_wrapping do
       legend_html <<
           hidden_field_for_all <<
           choices_group_wrapping do
-            collection.map {|choice|
+            collection.map { |choice|
               choice_wrapping(choice_wrapping_html_options(choice)) do
-                choice_nested_html(choice)
+                name, id = choice
+                choice_nested_html(@model.find(id))
               end
             }.join("\n").html_safe
           end
     end
   end
 
-  def choice_nested_html(choice, nested = false)
-    form_nested = nested ? "" : sub_choice_nested_html(choice)
-    choice_html(choice) << form_nested
+  def choice_nested_html(model, nested = false)
+    # show checkbox if "show_if" attribute is not provided
+    # else call method specified by "show_if" attribute
+    # and based on result show check_box
+    if options[:show_if].nil? || (options[:show_if].present? && model.send(options[:show_if].to_s))
+      choice_html([model.send(label_method), model.id])
+    else
+      "#{model.send(label_method)}".html_safe
+    end << sub_choice_nested_html(model)
   end
 
-  def sub_choice_nested_html(choice)
-    name, id = choice
-    model = sanitized_method_name.singularize.classify.constantize.find(id)
+  # recursive nesting
+  def sub_choice_nested_html(model)
     template.content_tag(:ul, model.children.collect do |child|
-        choice_nested_html([child.send(label_method), child.id], true)
-      end.join("\n").html_safe, {:style => "margin-left:20px", :class => "sub_item-#{choice.last} sub-item"}
+      choice_nested_html(child)
+    end.join("\n").html_safe, {:style => "margin-left: 35px", :class => "sub_item-#{model.id} sub-item"}
     ) unless model.leaf?
-    end
+  end
 end
