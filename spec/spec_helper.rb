1088e9c3bf869e0d9180febdb6beb1b30746ece0
XeeD <xeedus@gmail.com>
2012-07-26 21:01:44 +0200

    load shoulda matchers in prefork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,7 @@ Spork.prefork do
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
+  require 'shoulda/matchers/integrations/rspec'
   require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
