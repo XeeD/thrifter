1791d7f58d9b542d1cdddd1676671e34e78ffb3d
Fida <svains@gmail.com>
2012-07-26 16:57:14 +0200

    add new actions for ParamItem controller

--- app/controllers/admin/param_items_controller.rb
+++ app/controllers/admin/param_items_controller.rb
@@ -1,7 +1,43 @@
+# encoding: UTF-8
+
 module Admin
   class ParamItemsController < AdminController
     def new
-      param_template.param_items
+    end
+
+    def create
+      if param_item.save
+        redirect_to admin_param_template_url(param_template),
+                    notice: "Parametr #{param_item.name} byl vytvořen"
+      else
+        flash.now[:error] = "Chyba při vytváření parametru"
+        render "new"
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      if param_item.update_attributes(params[:param_item])
+        redirect_to admin_param_template_url(param_template),
+                    notice: "Parametr #{param_item.name} byl upraven"
+      else
+        flash.now[:error] = "Parametr nebylo možné uložit"
+        render "edit"
+      end
+    rescue ActiveRecord::RecordNotFound
+      flash[:error] = "Nelze najít daný parametr"
+      redirect_to admin_param_template_url(param_template)
+    end
+
+    def destroy
+      param_item.destroy
+      flash[:notice] = "Parametr #{param_item.name} byl smazána"
+    rescue ActiveRecord::RecordNotFound
+      flash[:error] = "Nelze najít daný parametr"
+    ensure
+      redirect_to admin_param_template_url(param_template)
     end
 
     private
@@ -13,15 +49,15 @@ module Admin
     helper_method :param_template
 
     def param_item
-      @param ||= params[:id] ?
+      @param_item ||= params[:id] ?
           param_template.param_items.find(params[:id]) :
-          param_template.param_items.new(params[:param])
+          param_template.param_items.new(params[:param_item])
     end
 
     helper_method :param_item
 
     def param_items
-      param_template.param_items
+      @param_items ||= param_template.param_items
     end
 
     helper_method :param_items
