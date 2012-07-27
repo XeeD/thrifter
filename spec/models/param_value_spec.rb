e5c6216160eee0c8da5085c08cbcaf0a2d75c60c
Fida <svains@gmail.com>
2012-07-27 16:09:02 +0200

    add ParamValue model spec test

---
+++ spec/models/param_value_spec.rb
@@ -0,0 +1,20 @@
+# encoding: utf-8
+require 'spec_helper'
+
+describe ParamValue do
+
+  # Associations
+  it { should belong_to(:product) }
+  it { should belong_to(:param_item) }
+
+  # Validations
+  # value
+  it { should validate_presence_of(:value) }
+  it { should ensure_length_of(:value).is_at_most(200) }
+
+  context "with valid attributes" do
+    it "should be valid" do
+      ParamValue.new(valid_param_value_attributes).should be_valid
+    end
+  end
+end
