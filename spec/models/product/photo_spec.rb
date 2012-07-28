02247ecc1a8a64efce42a1665c076089acd70a90
XeeD <xeedus@gmail.com>
2012-07-28 21:00:56 +0200

    change spec of Product::Photo model to check if image uploading works

--- spec/models/product/photo_spec.rb
+++ spec/models/product/photo_spec.rb
@@ -16,4 +16,16 @@ describe Product::Photo do
       Product::Photo.new(valid_product_photo_attributes).should be_valid
     end
   end
+
+  context "when saving with uploaded image file" do
+    def valid_product_photo_attributes_with_image
+      valid_product_photo_attributes.merge(image: File.new(Rails.root + "spec/fixtures/images/product.jpg"))
+    end
+
+    it "saves the image" do
+      product_photo = Product::Photo.new(valid_product_photo_attributes_with_image)
+      product_photo.save
+      File.exists?(product_photo.image.path).should be_true
+    end
+  end
 end
