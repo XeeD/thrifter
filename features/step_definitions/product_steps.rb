885b0d272b95afa4e14d57dc3218c12a3e0adcd1
Fida <svains@gmail.com>
2012-07-30 16:12:54 +0200

    add new step definitions for Product

--- features/step_definitions/product_steps.rb
+++ features/step_definitions/product_steps.rb
@@ -2,9 +2,23 @@
 
 # Given statements
 Pokud /^existuje produkt "(.*?)"$/ do |product_name|
-  fail "product #{product_name} doesn't exists" if Product.find_by_name(product_name).nil?
+  @product = Product.find_by_name(product_name)
+  fail "product #{product_name} doesn't exists" if @product.nil?
 end
 
+Pokud /^upravuji produkt "(.*?)"$/ do |product_name|
+  @product = Product.find_by_name(product_name)
+  raise "product #{product_name} not found" if @product.nil?
+  visit edit_admin_product_path(@product)
+end
+
+Pokud /^parametr "(.*?)" je přiřazen produktu$/ do |param_item_name|
+  @param_item = ParamItem.find_by_name(param_item_name)
+  raise "param item #{param_item_name} not found" if @param_item.blank?
+  unless @product.template_param_items.include?(@param_item)
+    raise "param item \"#{param_item_name}\" is not assigned to product"
+  end
+end
 
 # When statements
 
