5f921813d77fc76f5daa320ad20816f30a0670bf
Fida <svains@gmail.com>
2012-07-30 16:17:42 +0200

    remove parametrization association validations

--- spec/models/parametrization_spec.rb
+++ spec/models/parametrization_spec.rb
@@ -2,8 +2,4 @@
 require 'spec_helper'
 
 describe Parametrization do
-  # Associations
-  it { should belong_to(:product) }
-  it { should belong_to(:param_item) }
-  it { should belong_to(:param_value) }
 end
