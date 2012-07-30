115ff7ceffe6dc373cfc83f5837a2ee40c7df1e5
Fida <svains@gmail.com>
2012-07-30 13:13:14 +0200

    fix Brand validate uniquess url test

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -14,7 +14,10 @@ describe Brand do
   # url
   it { should validate_presence_of(:url) }
   it { should ensure_length_of(:url).is_at_most(30) }
-  it { should validate_uniqueness_of(:url) }
+  it {
+    Brand.create(valid_brand_attributes)
+    should validate_uniqueness_of(:url)
+  }
 
   context "with valid attributes" do
     it "should be valid" do
