5f2952ce2e9555e8c975274a3deb17e0cda1dd47
Fida <svains@gmail.com>
2012-07-30 16:10:29 +0200

    remove Parametrization association from Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,11 +12,8 @@ class Product < ActiveRecord::Base
   has_one  :preferred_categorization, class_name: "Categorization", conditions: {preferred: true}
   has_one  :preferred_category, class_name: "Category", through: :preferred_categorization, source: :category
   has_one  :param_template, through: :preferred_category
-  has_many :template_param_items, through: :param_template, class_name: "ParamItem"
-
-  has_many :parametrizations
-  has_many :param_items, through: :parametrizations
-  has_many :param_values, through: :parametrizations
+  has_many :param_items, through: :param_template, class_name: "ParamItem"
+  has_many :param_values, through: :template_param_items, class_name: "ParamValue"
 
   # Validations
   validates :name,
