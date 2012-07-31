568133a407aa2a65c77b604975ecd7ab35569ea9
Fida <svains@gmail.com>
2012-07-31 17:57:38 +0200

    add GET index spec test

--- spec/controllers/admin/product_param_items_controller_spec.rb
+++ spec/controllers/admin/product_param_items_controller_spec.rb
@@ -3,6 +3,19 @@ require 'spec_helper'
 
 module Admin
   describe ProductParamItemsController do
+    let(:product) { Product.find_by_name("LG GB3133TIJW") }
 
+    describe "GET index" do
+      def get_index
+        get :index, product_id: product.id
+      end
+
+      it "renders 'index' template" do
+        get_index
+        response.should render_template("index")
+      end
+
+      context "when rendering views"
+    end
   end
 end
