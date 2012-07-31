fc29c40b675cd73d5ab927368aceb172f5a34458
Fida <svains@gmail.com>
2012-07-31 16:53:16 +0200

    remove unused product step definition

--- features/step_definitions/product_steps.rb
+++ features/step_definitions/product_steps.rb
@@ -6,12 +6,6 @@ Pokud /^existuje produkt "(.*?)"$/ do |product_name|
   fail "product #{product_name} doesn't exists" if @product.nil?
 end
 
-Pokud /^upravuji produkt "(.*?)"$/ do |product_name|
-  @product = Product.find_by_name(product_name)
-  raise "product #{product_name} not found" if @product.nil?
-  visit edit_admin_product_path(@product)
-end
-
 Pokud /^parametr "(.*?)" je přiřazen produktu$/ do |param_item_name|
   @param_item = ParamItem.find_by_name(param_item_name)
   raise "param item #{param_item_name} not found" if @param_item.blank?
