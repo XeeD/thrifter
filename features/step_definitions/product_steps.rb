29537bbcdd863312dfe9b4c1820af792b9f286a0
XeeD <xeedus@gmail.com>
2012-08-02 18:04:29 +0200

    define some steps for feature regarding assignment of product to categories

--- features/step_definitions/product_steps.rb
+++ features/step_definitions/product_steps.rb
@@ -6,6 +6,18 @@ Pokud /^existuje produkt "(.*?)"$/ do |product_name|
   fail "product #{product_name} doesn't exists" if @product.nil?
 end
 
+Pokud /^jsem v editace produktu "(.*?)" na záložce "(.*?)"$/ do |product_name, tab_name|
+  @product = Product.find_by_name(product_name)
+  @product.should_not be_nil
+
+  visit case tab_name.strip
+          when "Kategorie"
+            admin_product_categorizations_path(@product)
+          else
+            raise "tab '#{tab_name}' is not known"
+        end
+end
+
 # When statements
 
 # Then statements
