98bdcb0a82e40e97f8e167708bfe7725b40658a6
XeeD <xeedus@gmail.com>
2012-08-06 11:52:15 +0200

    add IncludeMatch matcher that scans array and tries to match given Regexp

--- spec/support/custom_matchers.rb
+++ spec/support/custom_matchers.rb
@@ -1,3 +1,23 @@
 module CustomMatchers
   include RSpec::Rails::Matchers
+
+  class IncludeMatch
+    def initialize(expected)
+      expected = /#{Regexp.escape(expected)}/ unless expected.kind_of?(Regexp)
+      @expected = expected
+    end
+
+    def matches?(target)
+      @target = target
+      !target.find { |item| item =~ @expected }.nil?
+    end
+
+    def failure_message
+      "Expected #{@target.inspect} to include string that matches #{@expected}"
+    end
+  end
+
+  def include_match(expected)
+    IncludeMatch.new(expected)
+  end
 end
