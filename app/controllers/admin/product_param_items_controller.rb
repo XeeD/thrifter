4da0ed4d00ef84082090184457abd906ea47326d
XeeD <xeedus@gmail.com>
2012-08-06 09:56:25 +0200

    use Admin::ProductDecorator instead of Product

--- app/controllers/admin/product_param_items_controller.rb
+++ app/controllers/admin/product_param_items_controller.rb
@@ -16,31 +16,10 @@ module Admin
 
     private
 
-    def select_assigned_param_values
-      param_values = Product.joins(:defined_param_values).select("parametrizations.param_item_id, param_values.value")
-
-      param_values.inject({}) do |hash, h|
-        if hash.has_key?(h.param_item_id)
-          unless hash[h.param_item_id].kind_of?(Array)
-            hash[h.param_item_id] = [hash[h.param_item_id]]
-          end
-          hash[h.param_item_id] << h.value
-        else
-          hash[h.param_item_id] = h.value
-        end
-
-        hash
-      end
-    end
-
-    def assigned_param_values
-      @assigned_param_values ||= select_assigned_param_values
-    end
-
     helper_method :assigned_param_values
 
     def product
-      @product ||= Product.find(params[:product_id])
+      @product ||= ProductDecorator.find(params[:product_id])
     end
 
     helper_method :product
