f91b6090312ed53e487ee2ce7549a31faf632674
Fida <svains@gmail.com>
2012-08-07 14:40:02 +0200

    move sortable param items from helper to ParamTemplate decorator

--- app/decorators/admin/param_template_decorator.rb
+++ app/decorators/admin/param_template_decorator.rb
@@ -1,4 +1,10 @@
 class Admin::ParamTemplateDecorator < Draper::Base
   decorates :param_template
   decorates_association :param_items, with: Admin::ParamItemDecorator
+
+  def sorted_param_items_by_group_position
+    param_items.sort do |param_item_1, param_item_2|
+      param_item_1.param_group_position <=> param_item_2.param_group_position
+    end
+  end
 end
