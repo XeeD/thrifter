6b02ef177af824ab87761d848ecfdae49ef94e98
Fida <svains@gmail.com>
2012-08-10 15:55:06 +0200

    add NewsItems path to feature paths

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -7,6 +7,7 @@ def get_section_url(section)
     when "administrace kategorií" then admin_categories_path
     when "administrace šablon parametrů" then admin_param_templates_path
     when "administrace obchodů" then admin_shops_path
+    when "administrace novinek" then admin_news_items_path
     else raise "section url for '#{section}' was not found"
   end
 end
