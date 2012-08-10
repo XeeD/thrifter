0932bdeab1d8ed719878455ed949cc9b732bece6
Fida <svains@gmail.com>
2012-08-10 15:52:04 +0200

    add CreateArticles migration

---
+++ db/migrate/20120810095210_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles, force: true do |t|
+      t.string :title, limit: 150
+      t.string :url, limit: 150
+      t.string :state, limit: 10
+      t.text :content
+      t.text :summary
+      t.timestamps
+    end
+  end
+end
