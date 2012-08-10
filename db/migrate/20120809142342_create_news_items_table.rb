015860fc77ad29e9d9c5f44ed478a25c30793bbf
Fida <svains@gmail.com>
2012-08-10 15:51:07 +0200

    add CreateNewsItemsTable migration

---
+++ db/migrate/20120809142342_create_news_items_table.rb
@@ -0,0 +1,13 @@
+class CreateNewsItemsTable < ActiveRecord::Migration
+  def change
+    create_table :news_items, force: true do |t|
+      t.string :title, limit: 100
+      t.text :content
+      t.string :link, limit: 250, null: true
+      t.timestamps
+      t.belongs_to :shop
+    end
+
+    add_index :news_items, :shop_id
+  end
+end
