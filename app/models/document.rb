71591d2d0ee9eef3a7169ccb6e0c10f98135ebb4
Fida <svains@gmail.com>
2012-08-13 14:37:03 +0200

    add remove_old_contact_link before_save callback

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,6 +3,8 @@
 class Document < ActiveRecord::Base
   has_and_belongs_to_many :shops, join_table: :shop_documents
 
+  before_save :remove_old_contact_link
+
   validates :name,
             presence: true,
             length: {maximum: 70},
@@ -19,4 +21,13 @@ class Document < ActiveRecord::Base
 
   validates :content,
             presence: true
+
+  private
+
+  def remove_old_contact_link
+    if contact_link
+      # Only one document can be marked as contact_link
+      Document.update_all(contact_link: false)
+    end
+  end
 end
