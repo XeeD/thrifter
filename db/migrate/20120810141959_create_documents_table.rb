16d257219fb9822f70680eaab7396dbc274cee3a
Fida <svains@gmail.com>
2012-08-13 13:47:00 +0200

    add CreateDocumentsTable migration

--- db/migrate/20120810141959_create_documents_table.rb
+++ db/migrate/20120810141959_create_documents_table.rb
@@ -1,15 +1,16 @@
 class CreateDocumentsTable < ActiveRecord::Migration
   def change
     create_table :documents, force: true do |t|
-      t.string :name, limit: 150
-      t.string :url, limit: 150
+      t.string :title, limit: 250
+      t.string :name, limit: 70
+      t.string :url, limit: 70
       t.text   :content
-      t.string :state, limit: 10, default: "visible"
+      t.boolean :menu_item, default: false
+      t.boolean :contact_link, default: false
       t.timestamps
     end
 
     add_index :documents, :name, unique: true
-    add_index :documents, :url, unique: true
-    add_index :documents, :state
+    add_index :documents, :url,  unique: true
   end
 end
