2064588b5937e52de1f1729cd6bf0c865d3f3065
Fida <svains@gmail.com>
2012-08-13 17:13:26 +0200

    add Article model

---
+++ app/models/article.rb
@@ -0,0 +1,27 @@
+class Article < ActiveRecord::Base
+  # Associations
+  has_and_belongs_to_many :categories, join_table: :category_articles
+  has_many :shops
+
+  # Validations
+  validates :name,
+            presence: true,
+            length: {maximum: 100}
+
+  validates :url,
+            presence: true,
+            length: {maximum: 100}
+
+  validates :title,
+            presence: true,
+            length: {maximum: 250}
+
+  validates :content,
+            presence: true
+
+  validates :summary,
+            presence: true
+
+  validates :categories,
+            presence: true
+end
