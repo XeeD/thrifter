14e8d81c912ab1e84e1c400a67cfff650d1771d8
Fida <svains@gmail.com>
2012-08-13 17:25:27 +0200

    add new actions to Articles controller

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -3,5 +3,63 @@
 module Admin
   class ArticlesController < AdminController
 
+    def index
+    end
+
+    def new
+    end
+
+    def create
+      if article.save
+        redirect_to admin_shop_articles_url(shop), notice: "Článek #{article.name} byl vytvořen"
+      else
+        flash.now[:error] = "Chyba při vytváření článku"
+        render "new"
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      if article.update_attributes(params[:article])
+        redirect_to admin_shop_articles_url(shop), notice: "Článek #{article.name} byl upraven"
+      else
+        flash.now[:error] = "Článek nebylo možné uložit"
+        render "edit"
+      end
+    rescue ActiveRecord::RecordNotFound
+      flash[:error] = "Nelze najít daný článek"
+      redirect_to admin_shop_articles_url(shop)
+    end
+
+    def destroy
+      article.destroy
+      flash[:notice] = "Článek #{article.name} byl smazán"
+    rescue ActiveRecord::RecordNotFound
+      flash[:error] = "Nelze najít daný článek"
+    ensure
+      redirect_to admin_shop_articles_url(shop)
+    end
+
+    private
+
+    def article
+      @article ||= params[:id] ? Article.find(params[:id]) : Article.new(params[:article])
+    end
+
+    helper_method :article
+
+    def articles
+      @articles ||= shop.articles
+    end
+
+    helper_method :articles
+
+    def shop
+      @shop ||= Shop.find(params[:shop_id])
+    end
+
+    helper_method :shop
   end
 end
