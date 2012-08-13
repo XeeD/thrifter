28dd695737b90e37659365327633fc305368b046
Fida <svains@gmail.com>
2012-08-13 17:18:57 +0200

    add Article step definitions

---
+++ features/step_definitions/article_steps.rb
@@ -0,0 +1,32 @@
+# encoding: UTF-8
+
+# Given statement
+Pokud /^článek "(.+)" v obchodu "(.+)" existuje$/ do |article_name, shop_name|
+  shop = Shop.find_by_name(shop_name)
+  raise "shop '#{shop_name}' doesn't exist" if shop.nil?
+
+  @article = Article.find_by_name(article_name)
+  shop.articles.should include(@article)
+end
+
+Pokud /^mám otevřenou administraci článků pro obchod "(.+)"$/ do |shop_name|
+  shop = Shop.find_by_name(shop_name)
+  raise "shop '#{shop_name}' doesn't exist" if shop.nil?
+
+  visit admin_shop_articles_path(shop)
+end
+
+# When statements
+
+# Then statements
+Pak /^článek "(.*?)" by měl být smazán$/ do |article_name|
+  begin
+    find("#articles").should_not have_content("#{article_name}")
+  rescue Capybara::ElementNotFound
+    page.should have_selector(".empty_set")
+  end
+end
+
+Pak /^článek "(.*?)" by měl být (?:vytvořen|upraven)$/ do |article_name|
+  find("#articles").should have_content("#{article_name}")
+end
