ec3275fa2e8de8cad1b1815b8138de9761cee891
Fida <svains@gmail.com>
2012-08-13 13:54:18 +0200

    add new Document step definitions

---
+++ features/step_definitions/document_steps.rb
@@ -0,0 +1,37 @@
+# encoding: UTF-8
+
+# Given statements
+Pokud /^dokument "(.*?)" existuje$/ do |document_name|
+  @document = Document.find_by_name(document_name)
+  fail "document #{document_name} doesn't exists" if @document.nil?
+end
+
+Pokud /^jsem v editaci dokumentu "(.*?)"$/ do |document_name|
+  step "dokument \"#{document_name}\" existuje"
+  step 'jsem v sekci "administrace dokumentů"'
+  step 'kliknu na odkaz "Upravit"'
+end
+
+# When statements
+
+# Then statements
+
+Pak /^dokument "(.*?)" by měl být smazán$/ do |document_name|
+  begin
+    find("#documents").should_not have_content("#{document_name}")
+  rescue Capybara::ElementNotFound
+    page.should have_selector(".empty_set")
+  end
+end
+
+Pak /^dokument "(.*?)" by měl být (?:vytvořen|upraven)$/ do |document_name|
+  find("#documents").should have_content("#{document_name}")
+end
+
+Pak /^dokument "(.*?)" by měl být přiřazen obchodu "(.*?)"$/ do |document_name, shop_name|
+  Document.find_by_name(document_name).shops.pluck("name").should include(shop_name)
+end
+
+Pak /^dokument "(.*?)" by neměl být přiřazen obchodu "(.*?)"$/ do |document_name, shop_name|
+  Document.find_by_name(document_name).shops.pluck("name").should_not include(shop_name)
+end
