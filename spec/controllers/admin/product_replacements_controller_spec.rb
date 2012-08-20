e0f02aebbb92e9e63e261db38a1b0a6c81fe9551
Fida <svains@gmail.com>
2012-08-20 16:47:26 +0200

    make Product replacement spec pass

--- spec/controllers/admin/product_replacements_controller_spec.rb
+++ spec/controllers/admin/product_replacements_controller_spec.rb
@@ -5,7 +5,8 @@ module Admin
     fixtures(:products)
 
     let(:product) { ProductDecorator.new(products(:samsung_tv)) }
-    let(:replacement) { products(:lg_tv) }
+    let(:replacement_lg) { products(:lg_tv) }
+    let(:replacement_philips) { product(:philips) }
 
     before do
       ProductDecorator.stub(find: product)
@@ -25,7 +26,7 @@ module Admin
         render_views
 
         before do
-          product.stub_chain(:replacements).and_return([replacement])
+          product.stub_chain(:replacements).and_return([replacement_lg])
         end
 
         it "calls ProductDecorator.find" do
@@ -39,7 +40,7 @@ module Admin
         end
 
         it "calls product.replacements" do
-          product.should_receive(:replacements).and_return([replacement])
+          product.should_receive(:replacements).and_return([replacement_lg])
           get_index
         end
       end
@@ -47,12 +48,23 @@ module Admin
 
     describe "POST create" do
       def post_valid_attributes
-        post :create, product: {replacement_ids: ["1"]}
+        post :create, product: {replacement_ids: [replacement_lg.id, replacement_philips.id]}
+      end
+
+      it "updates product replacement ids" do
+        post_valid_attributes
       end
     end
 
     describe "DELETE destroy" do
-      def delete_replacement
+      before do
+        product.stub_chain(:replacements, :find).and_return([replacement_lg, replacement_philips])
+      end
+
+      context "with replacement id destroys one record" do
+        def delete_replacement
+          delete :destroy, id: replacement_lg.id
+        end
       end
     end
   end
