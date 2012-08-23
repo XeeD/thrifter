593a8cd2b7dc3cbd05020f081106969c3c48e6a8
Fida <svains@gmail.com>
2012-08-23 09:16:33 +0200

    add after and before destroy callback to Replacable

--- app/models/product/replacable.rb
+++ app/models/product/replacable.rb
@@ -1,6 +1,17 @@
 class Product::Replacable < ActiveRecord::Base
   self.table_name = "product_replacements"
 
+  after_destroy :change_product_state
+  after_create  :change_product_state
+
+  def change_product_state
+    unless original.replacements.size > 0
+      original.revert_replace
+    else
+      original.replace
+    end
+  end
+
   # Associations
   with_options(class_name: "Product") do |product|
     product.belongs_to :original, foreign_key: "product_id"
