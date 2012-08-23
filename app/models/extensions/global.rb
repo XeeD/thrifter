1705456e8af859c05a77c031782905f66a359f8d
Fida <svains@gmail.com>
2012-08-23 09:15:55 +0200

    refactor url transliteration

--- app/models/extensions/global.rb
+++ app/models/extensions/global.rb
@@ -6,8 +6,14 @@ module Extensions
       def transliterate_permalink(attribute)
         setter = "#{attribute}=".to_sym
 
+        to_url = ->(value) {
+          unless value.nil?
+            value.gsub(/_/, "").parameterize
+          end
+        }
+
         define_method setter do |value|
-          self[attribute] = value.parameterize
+          self[attribute] = to_url.call(value)
         end
 
         validates attribute, format: {with: /[a-z-]/}
