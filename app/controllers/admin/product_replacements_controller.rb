f54a023685529889515bc932f6819b2c24ace7b6
Fida <svains@gmail.com>
2012-08-23 09:13:43 +0200

    refactor Product replacement controller

--- app/controllers/admin/product_replacements_controller.rb
+++ app/controllers/admin/product_replacements_controller.rb
@@ -2,37 +2,47 @@
 
 module Admin
   class ProductReplacementsController < AdminController
+    before_filter :allow_only_visible_and_replaced
+
     def index
     end
 
     def create
-      product.replacement_ids += params[:product][:replacement_ids]
-
-      if product.replace! & product.save!
-        redirect_to admin_product_replacements_url(product), notice: ""
-      else
-        flash.now[:error] = "Chyba při nahrazování výrobku #{product.name}"
-        render :action => :index
+      params[:product][:replacement_ids].each do |replacement_id|
+        product.replacables.create!(replaced_by_id: replacement_id)
       end
+
+      redirect_to admin_product_replacements_url(product), :notice => "Produkt #{product.name} byl nahrazen"
     end
 
     def destroy
       if params[:id] == "all"
-        product.replacement_ids = []
+        product.replacements.destroy_all
+        flash[:notice] = "Náhrady byly odstraněny"
       else
-        product.replacement_ids -= [params[:id].to_i]
+        begin
+          product.replacements.destroy(Product.unscoped.find(params[:id]))
+          flash[:notice] = "Náhrada produktu byla odstraněna"
+        rescue ActiveRecord::RecordNotFound
+          flash[:error] = "Nelze najít danou náhradu"
+        end
       end
-
-      if product.replacement_ids.empty?
-        product.revert_replace!
-      end
-
-      product.save!
-
-      redirect_to :back
+      redirect_to admin_product_replacements_url(product)
     end
 
     private
+
+    def allow_only_visible_and_replaced
+      allowed_states = %(visible replaced)
+      unless allowed_states.include?(product.state)
+        flash[:error] = if product.state == "new"
+                          "Nelze nahradit nový (neodsouhlasený) produkt"
+                        else
+                          "Lze nahradit pouze normální nebo nahrazený produkt"
+                        end
+        redirect_to admin_product_url(product)
+      end
+    end
 
     def replacements
       @replacements ||= product.replacements
@@ -41,7 +51,7 @@ module Admin
     helper_method :replacements
 
     def product
-      @product ||= ProductDecorator.decorate Product.with_states(:visible, :replaced).find(params[:product_id])
+      @product ||= ProductDecorator.decorate Product.find(params[:product_id])
     end
 
     helper_method :product
