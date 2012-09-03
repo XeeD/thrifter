8e865cf82a3160f7277221de94bcb3e0aa23f724
Fida <svains@gmail.com>
2012-09-03 16:34:20 +0200

    refactor Amica miner

--- app/import/miners/amica.rb
+++ app/import/miners/amica.rb
@@ -2,28 +2,37 @@ require "parseexcel"
 
 module Miners
   class Amica < Base
-    SKIP_TO_ROW = 1
-    WORK_SHEET  = 0
+    SKIP_FIRST_ROWS = 1
+    BASE_WORK_SHEET  = 0
 
     def each
-      @xls.each(SKIP_TO_ROW) do |record_line|
-        yield Record.new
+      @xls.each(SKIP_FIRST_ROWS) do |record_line|
+        yield Record.new(record_line)
       end
     end
 
     AMICA_XLS_FILE = upload_path("amica.xls")
 
     def load_resource
-      @xls = parse_xls(AMICA_XLS_FILE).worksheet(WORK_SHEET)
+      @xls = parse_xls(AMICA_XLS_FILE).worksheet(BASE_WORK_SHEET)
     end
 
-    extracts_data :stock_availability
+    extracts_data :stock_availability, :supplier_items
 
     class Record < Base::XLSRecord
       extract_columns do
-        string  2 => :id
-        string  3 => :name
-        integer 5 => :in_stock_count
+        string  "B" => :id
+        string  "D" => :name
+        integer "E" => :in_stock_count
+        integer "F" => :ean
+      end
+
+      def name=(name)
+        self[:name] = name.gsub(/[^A-Za-z0-9 +.]/, '').strip
+      end
+
+      def in_stock_count=(count)
+        self[:in_stock_count] = count > 0 ? count : 0
       end
     end
   end
