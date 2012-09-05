d3307106f692dff3fc879bface7c4c51864fbd9d
Fida <svains@gmail.com>
2012-09-05 16:36:01 +0200

    use different layout for pjax request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  layout :set_layout_template
+
+  private
+
+  def set_layout_template
+    if request.headers['X-PJAX']
+      "pjax"
+    else
+      "application"
+    end
+  end
 end
