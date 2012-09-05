459a05179a54bd030919e5929afb76be0358dd33
Fida <svains@gmail.com>
2012-09-05 16:40:21 +0200

    add pjax-data for main menu (example)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,6 @@ class CategoriesController < ApplicationController
   def index
     @title = params[:category]
     @category_name = params[:category]
+    @pjax_data = {active_menu: params[:category]}
   end
 end
