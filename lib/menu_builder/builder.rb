6e9523dfbb24a5948821b3f8c09a4520a6fdcbbc
Fida <svains@gmail.com>
2012-09-05 16:38:05 +0200

    resolve module class loading error

--- lib/menu_builder/builder.rb
+++ lib/menu_builder/builder.rb
@@ -1,14 +1,12 @@
-module MenuBuilder
-  class Builder
-    include Rails.application.routes.url_helpers
+class MenuBuilder::Builder
+  include Rails.application.routes.url_helpers
 
-    def self.build(&builder_block)
-      builder = new
-      builder.instance_eval(&builder_block)
-    end
+  def self.build(&builder_block)
+    builder = new
+    builder.instance_eval(&builder_block)
+  end
 
-    def menu(name, &block)
-      Menu.new(name, &block)
-    end
+  def menu(name, &block)
+    MenuBuilder::Menu.new(name, &block)
   end
 end
