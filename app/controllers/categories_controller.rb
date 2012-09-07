c47d741b37da9661158600f8e238ab1dc736fd32
Fida <svains@gmail.com>
2012-09-07 16:37:23 +0200

    update frontend product and category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,12 @@
 class CategoriesController < ApplicationController
   def index
-    @title = params[:category_url]
-    @category_name = params[:category_url]
     set_pjax_data(:active_menu, params[:category_url])
-    set_pjax_data(:eval, "")
   end
+
+  private
+  def category
+    @category ||= Category.includes(:products).find_by_url(params[:category_url])
+  end
+
+  helper_method :category
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,17 @@
 class ProductsController < ApplicationController
   def show
-
+    if params[:section] && pjax_request?
+      render "section"
+    #else
+      #set_pjax_data(:eval, "$('a[data-pjax-remote]').pjax('[data-pjax-section-container]')")
+    end
   end
 
   private
 
   def product
-    Category.joins(:product).where("categories.url" => params[:category_url], "products.url" => params[:product_url])
+    @product ||= Product.find_by_url(params[:product_url])
   end
+
+  helper_method :product
 end
