d254bd182e31fa5a794c98c896f0e445c3e98461
Fida <svains@gmail.com>
2012-09-10 15:30:07 +0200

    refactor and simplify basic record classes

--- app/import/miners/base/xls_record.rb
+++ app/import/miners/base/xls_record.rb
@@ -1,32 +1,8 @@
 module Miners
   class Base
-    class XLSRecord < BasicRecord
+    class XLSRecord < SimpleRecord
       class << self
-        class Definition
-          attr_reader :record_class, :xls_sheet
-
-          def initialize(record_class, xls_sheet)
-            @record_class, @xls_sheet = record_class, xls_sheet
-          end
-
-          def self.transformation(name, &transform)
-            define_method name do |definition|
-              column    = definition.keys.first
-              attribute = definition.values.first
-
-              if column.is_a?(String)
-                column = ("a".."z").to_a.index(column.downcase)
-              end
-
-              record_class.define_simple_setter(attribute)
-
-              record_class.register_extractor(xls_sheet) do |record_xls|
-                value = instance_exec record_xls.at(column), &transform
-                send("#{attribute}=", value)
-              end
-            end
-          end
-
+        class Definition < ::Miners::Base::AbstractRecord::Definition
           transformation :integer do |value|
             value.to_i
           end
@@ -40,42 +16,16 @@ module Miners
           end
         end
 
-        def extract_columns(xls_sheet=:base, &definition)
-          Definition.new(self, xls_sheet).instance_exec(&definition)
-        end
-
-        def connect_xls_sheet(xls_sheet, &connector)
-          connected_xls_sheets << {
-              xls_sheet: xls_sheet,
-              connector: connector
+        def extract_records(resource_name=:base, &definition)
+          value_fetcher = ->(record, column) {
+            unless column.is_a?(Fixnum)
+              column = ("a" .. "z").to_a.index(column.to_s.downcase)
+            end
+            record.at(column)
           }
+
+          Definition.new(self, resource_name, value_fetcher).instance_exec(&definition)
         end
-
-        def connected_xls_sheets
-          @connected_xls_sheets ||= []
-        end
-      end
-
-      def initialize(base_xls, additional_xml_sheets={})
-        self.xls_sheets[:base] = base_xls
-
-        self.class.connected_xls_sheets.each do |connection|
-          xls = additional_xml_sheets[connection[:xls_sheet]]
-          xls_sheets[connection[:xls_sheet]] = connection[:connector].call(xls, self)
-        end
-
-        run_extractors
-      end
-
-      def run_extractors
-        self.class.extractors.each do |extractor|
-          xls_sheet, block = extractor
-          self.instance_exec(xls_sheets[xls_sheet], &block)
-        end
-      end
-
-      def xls_sheets
-        @xls_sheets ||= {}
       end
     end
   end
--- app/import/miners/base/xml_record.rb
+++ app/import/miners/base/xml_record.rb
@@ -1,37 +1,8 @@
 module Miners
   class Base
-    class XMLRecord < BasicRecord
+    class XMLRecord < SimpleRecord
       class << self
-        class Definition
-          attr_reader :record_class, :xml_name
-
-          def initialize(record_class, xml_name)
-            @record_class, @xml_name = record_class, xml_name
-          end
-
-          def self.transformation(name, &transform)
-            define_method name do |definition|
-              xpath = definition.keys.first
-              attribute = definition.values.first
-
-              record_class.define_simple_setter(attribute)
-
-              record_class.register_extractor(xml_name) do |record_xml|
-                # Execute transformation in context of Record instance
-                value = instance_exec record_xml.xpath(xpath).text.strip, &transform
-                send("#{attribute}=", value)
-              end
-            end
-          end
-
-          transformation :integer do |value|
-            value.to_i
-          end
-
-          transformation :string do |value|
-            value
-          end
-
+        class Definition < ::Miners::Base::AbstractRecord::Definition
           transformation :money do |value|
             value.gsub(/,/, '.').to_f.round
           end
@@ -43,48 +14,14 @@ module Miners
           end
         end
 
-        def extract_xpaths(xml_name=:base, &definition)
-          Definition.new(self, xml_name).instance_exec(&definition)
-        end
-
-        def connect_xml(xml_name, &connector)
-          connected_xmls << {
-              xml_name: xml_name,
-              connector: connector
+        def extract_records(resource_name=:base, &definition)
+          value_fetcher = ->(record, xpath) {
+            record.xpath(xpath).text.strip
           }
+
+          #super(resource_name, value_fetcher, &definition)
+          Definition.new(self, resource_name, value_fetcher).instance_exec(&definition)
         end
-
-        def connected_xmls
-          @connected_xmls ||= []
-        end
-      end
-
-      def initialize(base_xml, additional_xmls={})
-        self.xmls[:base] = base_xml
-
-        run_extractors(:only_base)
-
-        self.class.connected_xmls.each do |connection|
-          xml = additional_xmls[connection[:xml_name]]
-          xmls[connection[:xml_name]] = connection[:connector].call(xml, self)
-        end
-
-        run_extractors
-      end
-
-      def run_extractors(only_base=false)
-        self.class.extractors.send(only_base ? :select : :reject) { |extractor|
-          extractor[0] == :base
-        }.each do |extractor|
-          xml_name, block = extractor
-          # What to do, when associated record is not found?
-          next unless xmls[xml_name].present?
-          self.instance_exec(xmls[xml_name], &block)
-        end
-      end
-
-      def xmls
-        @xmls ||= {}
       end
     end
   end
