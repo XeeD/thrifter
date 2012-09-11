101fa8d53622cd31d37c26c7c5b29a057219bfd3
Fida <svains@gmail.com>
2012-09-11 16:46:28 +0200

    add shopping order helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,22 @@ class ApplicationController < ActionController::Base
   end
 
   protected
-    def pjax_request?
-      env['HTTP_X_PJAX'].present?
-    end
+
+  def has_shopping_order?
+    !session[:order_token].blank?
+  end
+
+  helper_method :has_shopping_order?
+
+  def shopping_order
+    @shopping_order ||= Order.find_by_token(session[:order_token]) if has_shopping_order?
+  end
+
+  helper_method :shopping_order
+
+  def pjax_request?
+    env['HTTP_X_PJAX'].present?
+  end
 
   private
 
