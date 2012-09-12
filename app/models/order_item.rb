e971479e154716f4b584c3ffc7732dd50b7827bc
Fida <svains@gmail.com>
2012-09-12 11:47:43 +0200

    update OrderItem model

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,8 +1,30 @@
 class OrderItem < ActiveRecord::Base
-  belongs_to :order
+  belongs_to :order, touch: true
   belongs_to :product
+
+  after_save    :update_order
+  after_destroy :update_order
+
+  validates :quantity,
+            numericality: {only_integer: true}
+
+  validates :price,
+            numericality: {only_integer: true}
+
+  delegate :name,
+           :model_name,
+           :url,
+           :default_price,
+
+           to: :product, prefix: true
 
   def total
     quantity * price
+  end
+  alias :cost :total
+
+  private
+  def update_order
+    order.update_order!
   end
 end
