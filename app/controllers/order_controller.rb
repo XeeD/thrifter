15b535276c3d62cadd22b51ba586ade76bfe887b
Fida <svains@gmail.com>
2012-09-13 16:39:37 +0200

    update Order and it's views

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,4 +1,21 @@
 # encoding: UTF-8
 
 class OrderController < ApplicationController
+
+  def show
+  end
+
+  private
+
+  def order
+    @order ||= begin
+      unless session[:order_token].blank?
+        OrderDecorator.find_by_token(session[:order_token])
+      else
+        redirect_to cart_path
+      end
+    end
+  end
+
+  helper_method :order
 end
