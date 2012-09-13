91c6792a5a179f103a08aabbee5ea742aa6b9b98
Fida <svains@gmail.com>
2012-09-13 16:36:39 +0200

    create ShippingMethods migration

--- db/migrate/20120912151041_create_shipping_methods.rb
+++ db/migrate/20120912151041_create_shipping_methods.rb
@@ -1,7 +1,10 @@
 class CreateShippingMethods < ActiveRecord::Migration
   def change
     create_table :shipping_methods, force: true do |t|
+      t.string :name, limit: 100
 
+      t.text :short_description
+      t.text :description
     end
   end
 end
