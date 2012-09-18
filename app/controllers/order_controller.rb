9025bf4dd1fc27c0ab19e9998f116db74d09a4c3
Fida <svains@gmail.com>
2012-09-18 13:56:20 +0200

    update Order controller

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -2,7 +2,20 @@
 
 class OrderController < ApplicationController
 
+  before_filter :enforce_order, except: [:completed]
+
   def show
+  end
+
+  def completed
+  end
+
+  def update
+    if order.update_attributes(params[:order])
+      redirect_to completed_order_path #next_step
+    else
+      render :show
+    end
   end
 
   private
@@ -18,4 +31,8 @@ class OrderController < ApplicationController
   end
 
   helper_method :order
+
+  def enforce_order
+    redirect_to cart_path unless order
+  end
 end
