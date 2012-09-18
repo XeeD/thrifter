ed4edd79f9ca2d7c42a5cae71526e2abde33f6dd
Fida <svains@gmail.com>
2012-09-18 15:40:17 +0200

    update PaymentMethods controller

--- app/controllers/admin/payment_methods_controller.rb
+++ app/controllers/admin/payment_methods_controller.rb
@@ -2,6 +2,59 @@
 
 module Admin
   class PaymentMethodsController < Admin::AdminController
+    def index
+    end
 
+    def new
+      payment_method.package_sizes.build
+    end
+
+    def create
+      if payment_method.save
+        redirect_to admin_payment_methods_url, :notice => "Typ platby #{payment_method.name} byl vytvořen"
+      else
+        flash.now[:error] = "Chyba při vytváření typu platby"
+        render "new"
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      if payment_method.update_attributes(params[:payment_method])
+        redirect_to admin_payment_methods_url,
+                    notice: "Typ platby #{payment_method.name} byl upraven"
+      else
+        flash.now[:error] = "Chyba při ukládání typu platby"
+        render "edit"
+      end
+    rescue ActiveRecord::RecordNotFound
+      flash[:error] = "Nelze najít daný typ platby"
+      redirect_to admin_payment_methods_url
+    end
+
+    def destroy
+      payment_method.destroy
+      flash[:notice] = "Typ platby #{payment_method.name} byl smazán"
+    rescue ActiveRecord::RecordNotFound
+      flash[:error] = "Nelze najít daný typ platby"
+    ensure
+      redirect_to admin_payment_methods_url
+    end
+
+    private
+
+    def payment_method
+      @payment_method ||= params[:id] ? PaymentMethod.find(params[:id]) : PaymentMethod.new(params[:payment_method])
+    end
+
+    helper_method :payment_method
+
+    def payment_methods
+      @payment_methods ||= PaymentMethod.all
+    end
+
+    helper_method :payment_methods
   end
 end
