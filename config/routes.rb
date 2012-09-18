0ea942447874e16351469b1ec07e554d52ccbf97
Fida <svains@gmail.com>
2012-09-18 16:37:30 +0200

    add new Order routes to edit and complete order

--- config/routes.rb
+++ config/routes.rb
@@ -63,8 +63,11 @@ Thrifter::Application.routes.draw do
   resource :cart, path: "kosik", controller: "cart", only: [:show, :create, :update, :destroy] do
     collection { get :add_product, path: "vlozeni" }
   end
-  resource :order, path: "objednavka", controller: "order", only: [:show, :update] do
-    collection { get :completed, path: "dokonceni" }
+  resource :order, path: "objednavka", controller: "order", path_names: {edit: "uprava"}, only: [:show, :update] do
+    collection {
+      put :complete
+      get :completed, path: "dokonceni"
+    }
   end
 
   match "/:category_url/:product_url(/:section)" => "products#show"
