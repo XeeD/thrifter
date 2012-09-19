26a77fb26e8297854581f79de05662b4c2334657
Fida <svains@gmail.com>
2012-09-19 15:53:30 +0200

    fix ShopHasShippingMethods table

--- db/migrate/20120912152818_create_shop_has_shipping_methods.rb
+++ db/migrate/20120912152818_create_shop_has_shipping_methods.rb
@@ -1,10 +1,10 @@
 class CreateShopHasShippingMethods < ActiveRecord::Migration
   def change
-    create_table :shop_has_payment_methods, force: true do |t|
+    create_table :shop_has_shipping_methods, force: true do |t|
       t.belongs_to :shop
-      t.belongs_to :payment_method
+      t.belongs_to :shipping_method
     end
 
-    add_index :shop_has_payment_methods, [:shop_id, :payment_method_id], {unique: true, name: "index_shop_on_shop_id_and_payment_method_id"}
+    add_index :shop_has_shipping_methods, [:shop_id, :shipping_method_id], {unique: true, name: "index_shop_on_shop_id_and_shipping_method_id"}
   end
 end
