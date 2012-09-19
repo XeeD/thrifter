edb4dedcb89ee88e20709aaa2cb552bbae828094
Fida <svains@gmail.com>
2012-09-19 16:36:58 +0200

    add new validations to PaymentMethod model

--- app/models/payment_method.rb
+++ app/models/payment_method.rb
@@ -1,7 +1,20 @@
 class PaymentMethod < ActiveRecord::Base
   has_many :payments
-  has_many :orders
+  has_many :orders, through: :payments
 
   has_and_belongs_to_many :shipping_methods, join_table: "shipping_method_has_payment_methods"
   has_and_belongs_to_many :shops, join_table: "shop_has_payment_methods"
+
+  validates :name,
+            presence: true,
+            length: {maximum: 100}
+
+  validates :short_description,
+            presence: true
+
+  validates :description,
+            presence: true
+
+  validates :shops,
+            presence: true
 end
