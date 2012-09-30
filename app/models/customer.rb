d3769e70f09f9663d38dfdb968618070b20e0224
Fida <svains@gmail.com>
2012-09-30 21:54:48 +0200

    add Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,36 @@
 class Customer < ActiveRecord::Base
-  has_many :orders
+  has_one :order
+  has_one :company
+
+  validates :first_name,
+            presence: true,
+            length: {maximum: 40}
+
+  validates :last_name,
+            presence: true,
+            length: {maximum: 60}
+
+  validates :email,
+            presence: true,
+            length: {maximum: 60}
+            #format: {with: /[a-z]/}
+
+  validates :phone,
+            presence: true,
+            length: {maximum: 20}
+
+  delegate :name,
+           :ico,
+           :dic,
+
+           to: :company, prefix: true
+
+  def full_name
+    first_name + ' ' + last_name
+  end
+
+  #alias company_new company
+  #def company
+  #  company_new || build_company
+  #end
 end
